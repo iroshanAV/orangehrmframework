387b4fa4a9f89f06a780b5651f22c32775b158e2
iroshan <iroshanav@gmail.com>
2021-10-07 00:03:34 +0530

    page factory added

--- src/main/java/com/btf/qa/pageObjects/AssignLeavePage.java
+++ src/main/java/com/btf/qa/pageObjects/AssignLeavePage.java
@@ -3,60 +3,80 @@ package com.btf.qa.pageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class AssignLeavePage {
 public WebDriver driver;
 	
 	public AssignLeavePage (WebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
 	
-	By empNameF = By.id("assignleave_txtEmployee_empName");
-	By leaveTypeF = By.id("assignleave_txtLeaveType");
-	By fromDateF = By.id("assignleave_txtFromDate");
-	By toDateF   = By.id("assignleave_txtToDate");
-	By commentF = By.id("assignleave_txtComment");
-	By durationF = By.id("assignleave_duration_duration");	
-	By calendarF = By.xpath("//*[@id=\"frmLeaveApply\"]/fieldset/ol/li[4]/img");
-	By calendarF1 = By.xpath("//*[@id=\"frmLeaveApply\"]/fieldset/ol/li[5]/img"); 
 	
+	@FindBy(id="assignleave_txtEmployee_empName")
+	WebElement empNameF;
 	
+	@FindBy(id="assignleave_txtLeaveType")
+	WebElement leaveTypeF;
 	
-	By assignB = By.id("assignBtn");
+	@FindBy(id="assignleave_txtFromDate")
+	WebElement fromDateF;
+	
+	@FindBy(id="assignleave_txtToDate")
+	WebElement toDateF;
+	
+	@FindBy(id="assignleave_txtComment")
+	WebElement commentF;
+	
+	@FindBy(id="assignleave_duration_duration")
+	WebElement durationF;
+	
+	@FindBy(xpath="//*[@id=\\\"frmLeaveApply\\\"]/fieldset/ol/li[4]/img")
+	WebElement calendarF;
+	
+	@FindBy(id="//*[@id=\\\"frmLeaveApply\\\"]/fieldset/ol/li[5]/img")
+	WebElement calendarF1;
+	
+	@FindBy(id="assignBtn")
+	WebElement assignB;
+	
+
 	
 	public WebElement getempNameF() {
-		return driver.findElement(empNameF);
+		return empNameF;
 	}
 	
 	public WebElement leaveTypeF() {
-		return driver.findElement(leaveTypeF);
+		return leaveTypeF;
 	}
 	
 	public WebElement getfromDateF() {
-		return driver.findElement(fromDateF);
+		return fromDateF;
 	}
 	
 	public WebElement gettoDateF() {
-		return driver.findElement(toDateF);
+		return toDateF;
 	}
 	
 	public WebElement getcommentF() {
-		return driver.findElement(commentF);
+		return commentF;
 	}
 	
 	public WebElement getassignB() {
-		return driver.findElement(assignB);
+		return assignB;
 	}
 	
 	public WebElement getDurationF() {
-		return driver.findElement(durationF);
+		return durationF;
 	}
 	
 	public WebElement getCalendarF() {
-		return driver.findElement(calendarF);
+		return calendarF;
 	}
 	
 	public WebElement getCalendarF1() {
-		return driver.findElement(calendarF1);
+		return calendarF1;
 	}
 }
